cce0801a60e3dcbb91fd57855a5c9017aafe8f9c
uigormarshall <uigor.silva@acad.ifma.edu.br>
2024-01-13 07:44:58 -0300

    feat: altera import e grid

--- game-of-life/public/js/game.js
+++ game-of-life/public/js/game.js
@@ -1,8 +1,8 @@
 import { Render } from "./render";
 function start() {
     console.log("Iniciando Game...");
-    const x = 120;
-    const y = 120;
+    const x = 100;
+    const y = 100;
     const randomize = true;
     const render = new Render(x, y, randomize);
     setInterval(render.nextGen.bind(render), 200);
